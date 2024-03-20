f7d0590ad6cef3490d1f4705271dbc41e4c582cb
MikeKutz <mkutz1492@gmail.com>
2024-03-20 15:41:34 -0400

    made more tolerant to live run

--- test/UT test sec.sql
+++ test/UT test sec.sql
@@ -1,26 +1,28 @@
+clear screen;
 set serveroutput on;
+alter session set current_schema=cSQL;
 declare
     s clob;
     p clob;
     
-    tt tokens_nt;
+    tt       cSQL.tokens_nt;
     err_code int;
     
-    a token_aggregator_obj := new token_aggregator_obj;
+    a cSQL.token_aggregator_obj := new cSQL.token_aggregator_obj;
     sql_txt clob;
 begin
-    s := ddlt_ras_ut.sample_security_class( 1 );
-    p := ddlt_ras.get_pattern( ddlt_ras.security_class );
+    s := cSQL.ddlt_ras_ut.sample_security_class( 1 );
+    p := cSQL.ddlt_ras.get_pattern( cSQL.ddlt_ras.security_class );
     dbms_output.put_line(s);
     dbms_output.put_line(p);
     
-    tt := ddlt_util.pattern_parser( s
+    tt := cSQL.ddlt_util.pattern_parser( s
                                     ,p
-                                    ,ddlt_ras.get_define( ddlt_ras.security_class )
+                                    ,cSQL.ddlt_ras.get_define( cSQL.ddlt_ras.security_class )
                                     ,sql_txt );
 
-    delete from ddlt_matched_tokens_temp;
-    insert into ddlt_matched_tokens_temp select * from table(tt);
+    delete from cSQL.ddlt_matched_tokens_temp;
+    insert into cSQL.ddlt_matched_tokens_temp select * from table(tt);
     
     for t in values of tt
     loop
@@ -30,5 +32,5 @@ begin
     dbms_output.put_line( a.json_txt );
 end;
 /
-select * from ddlt_matched_tokens_temp;
-select * from ddlt_tokens_temp;
+-- select * from cSQL.ddlt_matched_tokens_temp;
+-- select * from cSQL.ddlt_tokens_temp;
