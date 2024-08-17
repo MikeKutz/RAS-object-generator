77e0f3a974ebf972c4dbd5c05bd51580e4f777ff
MikeKutz <mkutz1492@gmail.com>
2024-08-17 14:48:22 -0400

    more file rearangement

---
+++ install_ras_commands.sql
@@ -0,0 +1,14 @@
+-- constants
+@@./DDLT_RAS.pks
+@@./DDLT_RAS.pkb
+
+-- syntax assertion utils
+@@./ras_acl_assertions.pks
+@@./ras_acl_assertions.pkb
+
+-- overloaded Syntax Type
+@@./ras_acl_syntax_t.pks
+@@./ras_acl_syntax_t.pkb
+
+-- actual build and register syntaxes
+@@./Blueprints/install.sql
